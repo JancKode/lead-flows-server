2dc5b47442bbffc5afcb51c6de279ca1c4cc76cc
JancKode <gyntlify@gmail.com>
2022-08-20 03:13:50 +0800

    fixing build error

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ app.use(cors());
 
 const PORT = process.env.PORT || 9000;
 
-app.use("/api/sms", smsService);
+
+app.use("/api/sms", async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+}, smsService);
 app.use("/api/create-payment-intent", stripeService);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
