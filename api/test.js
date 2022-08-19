f61bdc4dda2bd0cde67ef86717bcd30345a44e57
JancKode <gyntlify@gmail.com>
2022-08-20 02:33:19 +0800

    testing redirect

--- api/test.js
+++ api/test.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 const { v4 } = require('uuid');
 
-app.post('/api', (req, res) => {
+app.post('/api/testing', (req, res) => {
   const path = `/api/item/${v4()}`;
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
